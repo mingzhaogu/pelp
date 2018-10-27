3a45955463770e771db63fbb578145257e9cd71a
mingzhaogu <anniegu.mingzhao@gmail.com>
2018-10-27 01:09:04 -0700

    Include validations

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,14 +1,21 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
-const jsonwebtoken = require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
+const passport = require("passport");
 
 const User = require("../../models/User");
 const keys = require("../../config/keys");
+const validateRegisterInput = require("../../validation/register");
+const validateLoginInput = require("../../validation/login");
 
 const router = express.Router();
 
 router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
 
+// PRIVATE AUTH ROUTE
+router.get('/current', passport.authenticate('jwt', { session: false }), (req, res) => {
+    res.json({ msg: 'Success' });
+})
 
 // REGISTER NEW USERS
 router.post('/register', (req, res) => {
@@ -25,23 +32,22 @@ router.post('/register', (req, res) => {
                 return res.status(400).json(errors);
             } else {
                 // Otherwise create a new user
+                const { username, firstName, lastName, password } = req.body;
                 const newUser = new User({
-                    username: req.body.username,
-                    firstName: req.body.firstName,
-                    lastName: req.body.lastName,
-                    password: req.body.password
-                })
+                    username, firstName, lastName, password
+                });
 
                 bcrypt.genSalt(10, (err, salt) => {
                     bcrypt.hash(newUser.password, salt, (err, hash) => {
                         if (err) throw err;
+
                         newUser.password = hash;
                         newUser.save()
                             .then(user => {
                                 const payload = {
                                     id: user.id,
                                     username: user.username
-                                }
+                                };
 
                                 jwt.sign(payload, keys.secretOrKeys, { expiresIn: 3600 }, (err, token) => {
                                     res.json({
@@ -59,9 +65,9 @@ router.post('/register', (req, res) => {
 
 
 
-// REGISTER NEW USERS
-router.post('/register', (req, res) => {
-    const { errors, isValid } = validateRegisterInput(req.body);
+// LOGIN EXISTING USERS
+router.post('/login', (req, res) => {
+    const { errors, isValid } = validateLoginInput(req.body);
     
     if (!isValid) res.status(400).json(errors);
     
